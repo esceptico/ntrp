7395ba985c4aa5171541a69bbbad50837508e996
Timur Ganiev <t.ganiev@replika.ai>
2026-02-06 22:11:44 +0400

    naming

--- ntrp-ui/src/components/AsciiArt.tsx
+++ ntrp-ui/src/components/AsciiArt.tsx
@@ -5,17 +5,17 @@ import { useAccentColor } from "../hooks/index.js";
 const require = createRequire(import.meta.url);
 const { version: VERSION } = require("../../package.json");
 
-const LOGO_LINE_1 = "█▄ █ ▀█▀ █▀▄ █▀▄";
-const LOGO_LINE_2 = "█ ▀█  █  █▀▄ █▀▀";
+const LOGO_TOP = "█▄ █ ▀█▀ █▀▄ █▀▄";
+const LOGO_BOT = "█ ▀█  █  █▀▄ █▀▀";
 
 export function Welcome() {
   const { accentValue } = useAccentColor();
 
   return (
     <Box flexDirection="column" marginTop={1} marginLeft={1} marginBottom={1}>
-      <Text color={accentValue}>{LOGO_LINE_1}</Text>
+      <Text color={accentValue}>{LOGO_TOP}</Text>
       <Text>
-        <Text color={accentValue}>{LOGO_LINE_2}</Text>
+        <Text color={accentValue}>{LOGO_BOT}</Text>
         <Text dimColor> v{VERSION}</Text>
       </Text>
     </Box>
