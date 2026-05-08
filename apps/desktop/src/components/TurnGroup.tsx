0adb8ed11ba8c3b93d0487bc7f99e06dfb4579dd
Timur Ganiev <ganiev.tmr@gmail.com>
2026-05-08 04:49:39 +0400

    Tighten top padding above first work-block row

--- apps/desktop/src/components/TurnGroup.tsx
+++ apps/desktop/src/components/TurnGroup.tsx
@@ -65,7 +65,7 @@ export function TurnGroup({ userId, childIds }: { userId: string; childIds: stri
 
   const showInterim = !isDone || expanded;
   const interimList = (
-    <div className={clsx("flex flex-col gap-4", isDone && "pt-3.5")}>
+    <div className={clsx("flex flex-col gap-4", isDone && "pt-2")}>
       {layout.workIds.map((id) => (
         <Message key={id} id={id} isFinal={false} />
       ))}
